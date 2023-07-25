c570896ca9123adcd8795299a94ea14ecd1ad930
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-25 07:21:45 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+// const input = document.getElementById('input');
+// function addingEventListener() {
+//     alert('I was clicked!');
+// }
+// input.addEventListener('click', addingEventListener);
+
+
+function addingEventListener(){
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
 }
